5c7ba798def7431e39d11adf48f0f5e9f42fd5df
Kwang Yul Seo <kwangyul.seo@gmail.com>
2016-07-10 10:15:34 +0900

    Add a test case

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+import Test.Hspec
+
+import Data.Text.Zipper
+import qualified Data.Text.Zipper.Edit as Z
+
+tz = stringZipper ["abc", "def"] Nothing
+
+insertXAtTheBeginning = do
+  Z.moveCursor (0, 0)
+  Z.insertChar 'x'
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "execEdit" $ do
+    it "execute the edit session with the given zipper and return the modified zipper" $ do
+      let newTz = Z.execEdit insertXAtTheBeginning tz
+      getText newTz `shouldBe` ["xabc", "def"]
